f19992a74e5a29772a715123839cc426df98dbcf
begoczb <begocz.31@gmail.com>
2022-06-09 17:50:15 +0200

    add iteration 1 and 1.1

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,26 +1,62 @@
-  /**
-   * You might want to use this template to display each new characters
-   * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#examples
-   */
-  const characterTemplate = document.getElementById('template')
+/**
+ * You might want to use this template to display each new characters
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#examples
+ */
+const characterTemplate = document.getElementById("template");
+const baseUrl = "http://localhost:5005/api/characters";
 
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+const characterContainer = qs(".characters-container");
 
-  });
+const nameInput = qs("input[name=character-name]");
+console.log(nameInput);
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+document.getElementById("fetch-all").addEventListener("click", getCharacters);
 
-  });
+document.getElementById("fetch-one").addEventListener("click", getOneCharacter);
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+document
+  .getElementById("delete-one")
+  .addEventListener("click", function (event) {});
 
-  });
+document
+  .getElementById("edit-character-form")
+  .addEventListener("submit", function (event) {});
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+document
+  .getElementById("new-character-form")
+  .addEventListener("submit", function (event) {});
 
-  });
+async function getCharacters() {
+  const { data } = await axios.get(baseUrl);
+  createTemplateAndAppend(data.character);
+}
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+async function getOneCharacter(e) {
+  e.preventDefault();
+  const character = {
+    name: nameInput.value,
+  };
+  if (character.name === "") {
+    errorMessage.textContent = "Please provide some values !";
+    return;
+  }
+  const { data } = await axios.get(`${baseUrl}/${character.name}`);
+  createTemplateAndAppend(data[0]);
+}
 
-  });
+function createTemplateAndAppend(character) {
+  const clone = characterTemplate.content.cloneNode(true);
+  qs(".character-id", clone).textContent = character._id;
+  qs(".name", clone).textContent = character.name;
+  qs(".occupation", clone).textContent = character.occupation;
+  qs(".cartoon", clone).textContent = character.cartoon;
+  qs(".weapon", clone).textContent = character.weapon;
+  characterContainer.append(clone);
+}
 
+function qs(selector, element = document) {
+  return element.querySelector(selector);
+}
+function qsa(selector, element = document) {
+  return [...element.querySelectorAll(selector)];
+}
---
+++ routes/characters.routes.js
@@ -0,0 +1,93 @@
+const router = require("express").Router();
+const Character = require("../models/Character.model");
+const { isValidObjectId } = require("mongoose");
+
+/**
+ * !All the routes here are prefixed with /api/characters
+ */
+
+/**
+ * ? This route should respond with all the characters
+ */
+router.get("/", async (req, res, next) => {
+  /**Your code goes here */
+  try {
+    res.status(200).json(await Character.find());
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * ? This route should create one character and respond with
+ * ? the created character
+ */
+router.post("/", async (req, res, next) => {
+  /**Your code goes here */
+  try {
+    const { name, occupation, cartoon, weapon } = req.body;
+    if (!name || !occupation || !cartoon || !weapon) {
+      res.status(400).json({ message: `Please fill all the fields` });
+    }
+    if (
+      typeof name !== "string" ||
+      typeof occupation !== "string" ||
+      typeof cartoon !== "boolean" ||
+      typeof weapon !== "string"
+    ) {
+      res.status(400).json({ message: `Please enter the correct field type` });
+    }
+
+    const createdCharacter = await Character.create(req.body);
+    res.status(200).json(createdCharacter);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * ? This route should respond with one character
+ */
+router.get("/:name", async (req, res, next) => {
+  /**Your code goes here */
+  const character = await Character.find({ name: req.params.name });
+  res.status(200).json(character);
+});
+
+/**
+ * ? This route should update a character and respond with
+ * ? the updated character
+ */
+router.patch("/:id", async (req, res, next) => {
+  /**Your code goes here */
+  try {
+    const updatedCharacter = await Character.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedCharacter) {
+      res.status(404).json({ message: `No character with the specified id` });
+    }
+    res.status(200).json({ updatedCharacter });
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * ? Should delete a character and respond with a success or
+ * ? error message
+ */
+router.delete("/:id", async (req, res, next) => {
+  /**Your code goes here */
+  try {
+    await Character.findByIdAndDelete(req.params.id);
+    res.status(204).json({ message: `Succesfully deleted` });
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
